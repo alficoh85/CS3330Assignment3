384bfbd8426585d030c0e2461eb18b04c359cd39
WoodySmith2004 <bsmith_04@yahoo.com>
2025-04-01 14:33:02 -0500

    Starting up the factory

--- src/midicomposition/event/factory/MidiEventFactoryAbstract.java
+++ src/midicomposition/event/factory/MidiEventFactoryAbstract.java
@@ -1,5 +1,10 @@
 package midicomposition.event.factory;
 
-public interface MidiEventFactoryAbstract {
+import javax.sound.midi.MidiEvent;
+import java.io.*;
+import java.util.*;
 
+public interface MidiEventFactoryAbstract {
+	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
--- src/midicomposition/event/factory/StandardMidiEventFactory.java
+++ src/midicomposition/event/factory/StandardMidiEventFactory.java
@@ -1,5 +1,17 @@
 package midicomposition.event.factory;
 
-public class StandardMidiEventFactory implements MidiEventFactory {
+public class StandardMidiEventFactory implements MidiEventFactoryAbstract {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
