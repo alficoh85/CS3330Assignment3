54279abf1da6205705a141f76f6bc34b609aa616
Alex Cohen <alficoh85@gmail.com>
2025-04-04 04:07:43 -0500

    Updated MidiCsvParser with all the work I got done tonight

--- src/midicomposition/Main.java
+++ src/midicomposition/Main.java
@@ -1,14 +1,20 @@
 package midicomposition;
 
 import javax.sound.midi.*;
+
+import midicomposition.event.data.MidiCsvParser;
+import midicomposition.event.data.MidiEventData;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main (String [] args) {
+		try {
+			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./files/mystery_song.csv");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
--- src/midicomposition/event/data/MidiCsvParser.java
+++ src/midicomposition/event/data/MidiCsvParser.java
@@ -1,16 +1,26 @@
 package midicomposition.event.data;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MidiCsvParser {
 	public static List<MidiEventData> parseCsv(String filepath) {
+		List<MidiEventData> eventList = new ArrayList<MidiEventData>();
 		try {
-			
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			String line = br.readLine();
+            while (line != null) {
+                // TODO make new MidiEventData item with items from the line being read turned to ints
+                line = br.readLine();
+            }
+            return null;
 		}
-		catch (FileNotFoundException e) {
-			
+		catch (IOException e) {
+			e.printStackTrace();
+			return null;
 		}
-		return null;
 	}
 }
