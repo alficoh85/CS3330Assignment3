9e16d356a31d118549ead7bb2618af861eb25f0d
Alex Cohen <alficoh85@gmail.com>
2025-04-05 16:36:49 -0500

    Started work on main, added link to repository

--- src/midicomposition/Main.java
+++ src/midicomposition/Main.java
@@ -12,6 +12,8 @@ public class Main {
 	public static void main (String [] args) {
 		try {
 			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./files/mystery_song.csv");
+			Sequence sequence = new Sequence(Sequence.PPQ, 384);
+			Track track = sequence.createTrack();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
