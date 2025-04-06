00b898a7050f0dc77e693955b782693e98b9affa
Alex Cohen <alficoh85@gmail.com>
2025-04-05 20:00:40 -0500

    Finished and tested main, added extra credit answer to repo link txt file

--- src/midicomposition/Main.java
+++ src/midicomposition/Main.java
@@ -24,6 +24,34 @@ public class Main {
 			MidiEventFactory factory = factoryAbstract.createFactory();
 			
 			InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy();
+			instrumentStrategy.applyInstrument(track, 0);
+			instrumentStrategy = new TrumpetStrategy();
+			instrumentStrategy.applyInstrument(track, 1);
+			
+			PitchStrategy pitchStrategy = new HigherPitchStrategy();
+			
+			for (MidiEventData event : midiEvents) {
+				int modifiedNote = pitchStrategy.modifyPitch(event.getNote());
+				modifiedNote = pitchStrategy.modifyPitch(modifiedNote);
+				
+				if (event.getNoteOnOff() == ShortMessage.NOTE_ON) {
+					track.add(factory.createNoteOn(event.getStartEndTick(), modifiedNote, event.getVelocity(), event.getChannel()));
+				}
+				else {
+					track.add(factory.createNoteOff(event.getStartEndTick(), modifiedNote, event.getChannel()));
+				}
+			}
+			
+			Sequencer sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			sequencer.setSequence(sequence);
+			sequencer.start();
+			
+			while (sequencer.isRunning() | sequencer.isOpen()) {
+				Thread.sleep(100);
+			}
+			Thread.sleep(500);
+			sequencer.close();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
