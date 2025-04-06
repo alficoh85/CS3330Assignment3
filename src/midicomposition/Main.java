58c473870a3e5f977df503825d9f7b3861484e9d
Alex Cohen <alficoh85@gmail.com>
2025-04-05 19:27:03 -0500

    Fixed typos in 2 class names

--- src/midicomposition/Main.java
+++ src/midicomposition/Main.java
@@ -2,8 +2,10 @@ package midicomposition;
 
 import javax.sound.midi.*;
 
-import midicomposition.event.data.MidiCsvParser;
-import midicomposition.event.data.MidiEventData;
+import midicomposition.event.data.*;
+import midicomposition.event.factory.*;
+import midicomposition.strategy.instrument.*;
+import midicomposition.strategy.pitch.*;
 
 import java.io.*;
 import java.util.*;
@@ -14,6 +16,14 @@ public class Main {
 			List<MidiEventData> midiEvents = MidiCsvParser.parseCsv("./files/mystery_song.csv");
 			Sequence sequence = new Sequence(Sequence.PPQ, 384);
 			Track track = sequence.createTrack();
+			
+			MidiEventFactoryAbstract factoryAbstract = new StandardMidiEventFactoryAbstract();
+			//MidiEventFactoryAbstract factoryAbstract = new LegatoMidiEventFactoryAbstract();
+			//MidiEventFactoryAbstract factoryAbstract = new StaccatoMidiEventFactoryAbstract();
+			
+			MidiEventFactory factory = factoryAbstract.createFactory();
+			
+			InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
